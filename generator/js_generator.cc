0f35d29d6aa81512c4770803678ce64cb0d98709
Noel Kim (김민혁) <noel@chequer.io>
2022-04-29 14:06:53 +0900

    chore(typo): add line break in clear map method

--- generator/js_generator.cc
+++ generator/js_generator.cc
@@ -2818,7 +2818,7 @@ void Generator::GenerateClassField(const GeneratorOptions& options,
         " */\n"
         "$class$.prototype.$clearername$ = function() {\n"
         "  this.$gettername$().clear();\n"
-        "  return this;"
+        "  return this;\n"
         "};\n"
         "\n"
         "\n",
